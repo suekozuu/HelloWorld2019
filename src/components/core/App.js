b79aec8b9f63993076217bb19a265589e9f16a44
sueshi <suekozuu@gmail.com>
2019-08-26 16:58:28 +0700

    add app.js

---
+++ src/components/core/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar';
+import styled from 'styled-components'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
