691b22e90d6e299835c2bae9f56797fb6849f7a0
sueshi <suekozuu@gmail.com>
2019-08-26 19:55:59 +0700

    edit use all components in page

--- src/components/core/App.js
+++ src/components/core/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Navbar';
+import Title from './Title';
+import HomeImage from './HomeImage'
+import Shop from './Shop'
 import styled from 'styled-components'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <Navbar/>
+        <Title/>
+        <Shop/>
+        <HomeImage/>
+      </div>
     </div>
   );
 }
